cfedb15b10dabb973e59a4b7f5950c942dbf9b14
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-14 11:14:35 +0900

    genprjdir.py

---
+++ genprjdir.py
@@ -0,0 +1,8 @@
+from os import rename as __mv__
+from os import remove as __rm__
+from os import chdir as __cd__
+
+__mv__('pyximportibler', 'pyximportible')
+__cd__('pyximportible')
+__rm__('
+__cd__('..')
